0d9e9581cddc5f3e32f3190e2562bda7e047be5e
MakerDZ <mcskb91@gmail.com>
2023-11-01 19:21:02 +0630

    only the last section left for home page

--- components/display/pages/HomePage.tsx
+++ components/display/pages/HomePage.tsx
@@ -16,8 +16,8 @@ export default function HomePage(){
                 </div>
                 <h1 className="font-extrabold xl:text-3xl md:text-2xl text-[1.375rem] sm:pt-14 pt-7 z-30 sm:w-auto w-80"><span className="text-[#7B57FC] font-black">Are You</span> Techie, Coder, UI/UXer?</h1>
             </div>
-            <p className="text-[#4E4E4E] text-lg sm:max-w-[400px] pt-6 sm:text-center sm:mx-auto font-medium">ကိုယ့် project ​​လေးတွေ ကိုယ်တိုင်လုပ်​နေကြတဲ့ သူ​တွေ အားလုံးအတွက် project ​​လေးတွေကို mentally secure ဖြစ်ဖြစ်နဲ့ sharing ​ကောင်း​ကောင်းလုပ်လို့ရနိုင်မဲ့ ​နေရာ​လေးတစ်ခုကို ဖန်တီးထားပါတယ်။</p>
-            <div className="pt-5">
+            <p className="text-[#4E4E4E] text-lg sm:max-w-[400px] w-full pt-6 sm:text-center sm:mx-auto font-medium">ကိုယ့် project ​​လေးတွေ ကိုယ်တိုင်လုပ်​နေကြတဲ့ သူ​တွေ အားလုံးအတွက် project ​​လေးတွေကို mentally secure ဖြစ်ဖြစ်နဲ့ sharing ​ကောင်း​ကောင်းလုပ်လို့ရနိုင်မဲ့ ​နေရာ​လေးတစ်ခုကို ဖန်တီးထားပါတယ်။</p>
+            <div className="sm:pt-5">
                 <div className='absolute sm:w-72 w-60 h-auto xl:left-[40%] lg:left-[37%] md:left-[33%] sm:left-[28%] left-[25%] sm:-mt-12 -mt-8 -z-10'>
                     <Image
                     src="/imageBg.png"
@@ -38,6 +38,16 @@ export default function HomePage(){
                     className='w-full h-auto rounded-md border-[#CDCDCD] border-1'
                     />
                 </div>
+            </div>
+            <div className='mx-auto sm:w-[460px] w-full py-12 z-10'>
+                <Image
+                src="/benefits.svg"
+                alt="The Benefits"
+                width="0"
+                height="0"
+                sizes="100vw"
+                className='w-full h-auto rounded-md'
+                />
             </div>
         </div>
     )
